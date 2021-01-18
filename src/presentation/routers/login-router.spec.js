9518440301b7195c0c51b3c2c508ac0172591630
Thiago Martins de Freitas <martins86@gmail.com>
2021-01-18 17:21:38 -0300

    ensure LoginRouter returns 400 if no password

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,22 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+
+    // Assert
+    const httpResponse = sut.route(httpRequest)
+
+    // Act
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    // Arrange
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@.com'
       }
     }
 
