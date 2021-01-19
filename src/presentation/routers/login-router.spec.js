f92c505c1ee301ab4e653ae5e64c0d89e965bfbc
Thiago Martins de Freitas <martins86@gmail.com>
2021-01-19 18:20:02 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,6 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('./../helpers/missing-param-error')
+const UnauthorizedError = require('./../helpers/unauthorized-error')
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -108,5 +109,6 @@ describe('Login Router', () => {
 
     // Act
     expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
 })
