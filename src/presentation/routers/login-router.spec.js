0c6934d576c25ae9217c1b357fdd70526ecc9707
Thiago Martins de Freitas <martins86@gmail.com>
2021-01-22 16:23:21 -0300

    ensure LoginRouter calls emailvalidator with correct email

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -19,6 +19,7 @@ const makeSut = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -301,5 +302,22 @@ describe('Login Router', () => {
 
     // Act
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    // Arrange
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    // Assert
+    await sut.route(httpRequest)
+
+    // Act
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
