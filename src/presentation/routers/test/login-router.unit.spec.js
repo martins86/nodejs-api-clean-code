fb0c48ad27e9cc55d9dae2c58340992cb0b3d84d
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-23 16:34:16 -0300

    create custom configs to unit and integration tests

--- src/presentation/routers/test/login-router.unit.spec.js
+++ src/presentation/routers/test/login-router.unit.spec.js
@@ -1,8 +1,8 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('./../helpers/missing-param-error')
-const InvalidParamError = require('./../helpers/invalid-param-error')
-const UnauthorizedError = require('./../helpers/unauthorized-error')
-const ServerError = require('./../helpers/server-error')
+const MissingParamError = require('../helpers/missing-param-error')
+const InvalidParamError = require('../helpers/invalid-param-error')
+const UnauthorizedError = require('../helpers/unauthorized-error')
+const ServerError = require('../helpers/server-error')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
