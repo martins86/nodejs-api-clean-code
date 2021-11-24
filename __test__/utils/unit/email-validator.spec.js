d28ec29a49290b03a8936beb60ff5941fc961f4a
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-24 11:14:47 -0300

    ensure EmailValidator calls validator with correct email

--- __test__/utils/unit/email-validator.spec.js
+++ __test__/utils/unit/email-validator.spec.js
@@ -6,10 +6,14 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should return true if validator return true', () => {
     // Arrange
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
 
     // Act & Assert
@@ -19,10 +23,21 @@ describe('Email Validator', () => {
   test('Should return false if validator return false', () => {
     // Arrange
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email')
 
     // Act & Assert
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    // Arrange
+    const sut = makeSut()
+
+    // Act
+    sut.isValid('any_email@email.com')
+
+    // Assert
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
