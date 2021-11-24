e562510aaf87e20d02925bde0dd95612a4dc4edf
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-24 13:26:09 -0300

    ensure AuthUseCase throws MissingParamError if no password is provided

--- __test__/domain/usecases/auth-usecase.spec.js
+++ __test__/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,13 @@
 const { MissingParamError } = require('../../../src/utils/errors')
 
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
@@ -18,5 +22,16 @@ describe('Auth UseCase', () => {
 
     // Assert
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', async () => {
+    // Arrange
+    const sut = new AuthUseCase()
+
+    // Act
+    const promise = sut.auth('any_email@email.com')
+
+    // Assert
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
