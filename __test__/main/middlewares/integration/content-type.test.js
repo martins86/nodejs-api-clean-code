7614a5b6e2d7aaaa2c7da3629f7c05f4fa17cca9
martins86 <martins86@gmail.com>
2021-12-28 14:39:39 +0000

    ensure requests return content-type xml is forced

--- __test__/main/middlewares/integration/content-type.test.js
+++ __test__/main/middlewares/integration/content-type.test.js
@@ -13,4 +13,17 @@ describe('Content-Type Middleware', () => {
       .get('/test-content-type')
       .expect('content-type', /json/)
   })
+
+  test("Should return xml content-type if forced", async () => {
+    // Arrange
+    app.get("/test-content-type-xml", (req, res) => {
+      res.type('xml')
+      res.send("")
+    })
+
+    // Act & Assert
+    await request(app)
+      .get("/test-content-type-xml")
+      .expect("content-type", /xml/)
+  })
 })
